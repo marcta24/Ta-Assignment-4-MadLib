ba3a6a86b3baf93cb242af9a565f04a06b92c711
marcta24 <49898206+marcta24@users.noreply.github.com>
2019-05-21 10:18:20 -0700

    Add files via upload

---
+++ MadLib/pizza.js
@@ -0,0 +1,22 @@
+userName=prompt("Enter your name")  
+userNoun=prompt("Enter a noun")
+userVerb=prompt("Enter a verb")
+userAdjective=prompt("Enter a Adjective")
+userColor=prompt("Enter a Color") 
+
+
+
+var userNoun;
+var userVerb;
+var userName;
+var userAdjective;
+var userColor;
+
+document.write("Pizza was invented by a chef named"+" "+ userName +"!"+ " "+"To make a pizza you need to take a lump of" +" "+ userNoun +" "+"and make a thin, round" + " "
++ userVerb+"."+"Then you cover it with"+" "+ userAdjective +" "+"sauce." +" "+ "The pizza might look funky but dont worry, it will turn" +" "+ userColor)
+
+
+
+
+
+
